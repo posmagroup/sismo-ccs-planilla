05b13d1e4eaf8a8d2e2e297b7393b93bb1b4c248
Anto <anto@anto.(none)>
2013-07-31 15:51:18 -0430

    Documentacion del Admin

--- webapp/apps/inspeccion/forms.py
+++ webapp/apps/inspeccion/forms.py
@@ -7,20 +7,26 @@ from widgets import Select_Polygon_Widget
 
 class RequiredInlineFormSet(BaseInlineFormSet):
     """
-    Generates an inline formset that is required
+        Clase que permite que las validaciones de los
+        modelos que se colocan como inline en el
+        admin de inspeccion funcionen.
     """
 
     def _construct_form(self, i, **kwargs):
         """
         Override the method to change the form attribute empty_permitted
         """
-
         form = super(RequiredInlineFormSet, self)._construct_form(i, **kwargs)
         form.empty_permitted = False
         return form
 
 
 class EstructuraInlineForm(ModelForm):
+    """
+        Clase que permite donde se especifica
+        el widget con el que se va a renderizar
+        el atributo poligo del modelo Estructura.
+    """
 
     def __init__(self, *args, **kwargs):
         super(EstructuraInlineForm, self).__init__(*args, **kwargs)
